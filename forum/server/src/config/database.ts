be691011089fcc5e1143abcd9931a6091e4a6e7c
Dhruv Patel <dhruvthakorpatel05@gmail.com>
2025-10-18 15:05:04 -0500

    script setup

--- forum/server/src/config/database.ts
+++ forum/server/src/config/database.ts
@@ -1,10 +1,10 @@
-// /src/config/database.ts
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const uri: string = process.env.ATLAS_URI || "";
+const uri: string = process.env.MONGO_URI || "";
+console.log("MongoDB URI:", uri);
 
 if (!uri) {
   console.error("MongoDB URI is not set in the environment variables.");
@@ -12,14 +12,13 @@ if (!uri) {
 }
 
 const connectToDatabase = async () => {
-    const mongoUri = process.env.ATLAS_URI || '';
-    try {
-      await mongoose.connect(mongoUri);
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('MongoDB connection error:', error);
-      process.exit(1); // Exit the process with failure
-    }
-  };
-  
-  export default connectToDatabase;
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+export default connectToDatabase;
